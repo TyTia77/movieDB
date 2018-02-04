5f11174b223a641d85d98d69f08a7923bf3497e5
Ty Tia <tytia77@gmail.com>
2018-02-04 14:27:49 +1100

    cleanup code and added some style changes to loading component

--- src/components/re-usable_components/loading_screen/loading-screen.jsx
+++ src/components/re-usable_components/loading_screen/loading-screen.jsx
@@ -1,16 +1,13 @@
-import React, { PropTypes } from "react"
+import React from "react"
 
 require('./loading-screen.scss')
 
-const Loader = () =>
+const Loader = () => 
     <div className="load-container">
         <div class="spinner">
             <div class="cube1"></div>
             <div class="cube2"></div>
         </div>
     </div>
-
-Loader.PropTypes = {
-}
 
 export default Loader
